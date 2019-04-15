0f1f590930acc1408095ad503af8fc52e4fb5759
ddrohan <ddrohan@wit.ie>
2019-04-15 22:53:47 +0100

    Lab 02 + Exercises Complete

--- app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -2,34 +2,55 @@ package org.wit.placemark.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_placemark.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import org.jetbrains.anko.toast
 import org.wit.placemark.R
-import org.wit.placemarks.models.PlacemarkModel
+import org.wit.placemark.main.MainApp
+import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger {
 
     var placemark = PlacemarkModel()
-    val placemarks = ArrayList<PlacemarkModel>()
+    lateinit var app : MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+
+
+        app = application as MainApp
 
         btnAdd.setOnClickListener() {
             placemark.title = placemarkTitle.text.toString()
             placemark.description = placemarkDescription.text.toString()
 
-            if (placemark.title.isNotEmpty() &&  placemark.description.isNotEmpty()) {
-                placemarks.add(placemark.copy())
-                placemarks.forEach { info("added: ${it.title} , ${it.description}")}
-            }
-            else {
-                toast ("Please Enter a title")
+            if (placemark.title.isNotEmpty()) {
+                app.placemarks.add(placemark.copy())
+                info("add Button Pressed: $placemarkTitle")
+                app.placemarks.forEach { info("add Button Pressed: ${it}")}
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
             }
         }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
---
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -0,0 +1,42 @@
+package org.wit.placemark.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+
+import android.view.*
+import kotlinx.android.synthetic.main.activity_placemark_list.*
+import org.jetbrains.anko.startActivityForResult
+import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
+
+class PlacemarkListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_placemark_list)
+        toolbarMain.title = title
+        setSupportActionBar(toolbarMain)
+
+        app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = PlacemarkAdapter(app.placemarks)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_add -> startActivityForResult<PlacemarkActivity>(0)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
+
